4c2dd8a243a3813db6e540cd84c70e09ea207dc1
Olya Glushkova <82057665+oglsh@users.noreply.github.com>
2022-03-21 01:16:48 +0300

    Update todos.js

--- routes/todos.js
+++ routes/todos.js
@@ -26,7 +26,6 @@ router.post('/login', urlencodedParser, (req, res) => {
     var cryptPassword = crypto.createHash("sha1").update(password).digest()
 
 
-     let userr = {
          login: login,
          password: cryptPassword.toString('hex')
      }
